0260f33b616ea011a96a80454537b99ff7cb8d80
RZen <bartellbe@gmail.com>
2023-06-12 13:28:23 +0000

    Fix Makefile Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('PGNAME'),
         'USER': os.getenv('PGUSER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
