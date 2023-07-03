0c7972b61fb0d59fadeb0e15010a4447eaabfe9e
RSen <bartellbe@gmail.com>
2023-07-04 00:09:22 +0600

    Update .gitignore settings/

--- task_manager/settings/__init__.py
+++ task_manager/settings/__init__.py
@@ -1,10 +1,10 @@
 from .base import *
+import os
 
-try:
-    from .development import *
-    live = False
-except ImportError:
-    live = True
-
-if live:
+if os.getenv('ACTION_MODE'):
+    try:
+        from .development import *
+    except ImportError:
+        from .production import *
+else:
     from .production import *
